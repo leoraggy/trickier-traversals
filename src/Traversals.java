cf8541061e6bcb1086059d406d50a034c00a240c
leoraggy <leoragual35@gmail.com>
2026-02-19 14:50:37 -0800

    Completed countInternalNodes

--- src/Traversals.java
+++ src/Traversals.java
@@ -31,7 +31,16 @@ public class Traversals {
    * @return the count of internal nodes, or 0 if the tree is null
    */
   public static int countInternalNodes(TreeNode<Integer> node) {
-    return 0;
+    if(node == null){
+      return 0;
+    }
+
+    if(node.left == null && node.right == null){
+      return 0;
+    }
+
+    return 1 + countInternalNodes(node.left) + countInternalNodes(node.right);
+
   }
 
   /**
